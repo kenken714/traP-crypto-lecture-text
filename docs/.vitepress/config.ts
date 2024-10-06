70e983e4852535ab96b25283506dc5fd3c8d2359
kenken714 <kenkenkeiko043@gmail.com>
2024-10-06 22:32:35 +0900

    init

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,52 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Crypto&暗号講習会",
+  description: "Crypto&現代暗号講習会のテキスト",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: '検索',
+            buttonAriaLabel: '検索ボックスを開く'
+          },
+          modal: {
+            displayDetails: '詳細を表示',
+            resetButtonTitle: 'リセット',
+            backButtonTitle: '戻る',
+            noResultsText: '見つかりませんでした',
+            footer: {
+              selectText: '選択',
+              selectKeyAriaLabel: '結果を選択するには、上下キーを使用します',
+              navigateText: '移動',
+              navigateUpKeyAriaLabel: '前の結果に移動するには、上キーを使用します',
+              navigateDownKeyAriaLabel: '次の結果に移動するには、下キーを使用します',
+              closeText: '閉じる'
+            }
+          }
+        }
+      }
+    }
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,18 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Crypto&暗号講習会"
+  text: "Crypto&現代暗号講習会のテキスト"
+  tagline: 23B @kenken
+
+features:
+  - title: 第一章
+    details: 暗号についての概要と環境構築をします(WIP)
+  - title: 第二章
+    details: 未定
+  - title: 第三章
+    details: 未定
+---
+
