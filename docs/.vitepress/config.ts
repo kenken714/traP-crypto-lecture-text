f65f5ce5f7735dee8c0ce8c74be7eaee467c8ae9
kenken714 <kenkenkeiko043@gmail.com>
2024-10-07 14:28:50 +0900

    update chapter 0
    だいたい完成した

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/kenken714/traP-crypto-lecture-text' }
     ],
     search: {
       provider: 'local',
